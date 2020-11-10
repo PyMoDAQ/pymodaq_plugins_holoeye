09f3f80acfc7ad0f3200b54d0d399436eba5033d
Sébastien Weber <seba.weber@gmail.com>
2020-11-10 13:54:43 +0100

    updated calls to new pymodaq version

--- pymodaq_plugins_holoeye/daq_move_plugins/daq_move_Holoeye.py
+++ pymodaq_plugins_holoeye/daq_move_plugins/daq_move_Holoeye.py
@@ -4,12 +4,13 @@ from PyQt5 import QtWidgets
 import sys
 sys.path.append(os.getenv("HEDS_PYTHON_MODULES",""))
 from holoeye import slmdisplaysdk
-from pymodaq.daq_utils.daq_utils import select_file
+from pymodaq.daq_utils.gui_utils import select_file
 from pymodaq.daq_move.utility_classes import DAQ_Move_base
 from pymodaq.daq_move.utility_classes import comon_parameters
 from pymodaq.daq_utils.daq_utils import ThreadCommand, getLineInfo
 
-from pymodaq.daq_utils import daq_utils, h5browser
+from pymodaq.daq_utils import daq_utils
+from pymodaq.daq_utils.h5modules import browse_data
 from easydict import EasyDict as edict
 from enum import IntEnum
 import tables
@@ -27,8 +28,6 @@ class HoloeyeControls(IntEnum):
         return [name for name, member in cls.__members__.items()]
 
 
-
-
 class DAQ_Move_Holoeye(DAQ_Move_base):
     """
         Wrapper object to access the Mock fonctionnalities, similar wrapper for all controllers.
@@ -38,31 +37,39 @@ class DAQ_Move_Holoeye(DAQ_Move_base):
         *params*          dictionnary
         =============== ==============
     """
-    is_multiaxes=False
-    stage_names=[]
+    is_multiaxes = False
+    stage_names = []
 
-    _controller_units = 'greyscale' #dependent on the shaping_type so to be updated accordingly using self.controller_units = new_unit
+    _controller_units = 'greyscale'  # dependent on the shaping_type so to be updated accordingly using self.controller_units = new_unit
 
-    params= [{'title': 'Show Preview?:', 'name': 'show_preview', 'type': 'bool', 'value': False},
-             {'title': 'Shaping type:', 'name': 'shaping_type', 'type': 'list', 'value': HoloeyeControls.names()[2],  'values': HoloeyeControls.names()},
-             {'title': 'Splitting options:', 'name': 'splitting', 'type': 'group','visible': True, 'children':[
-                 {'title': 'Splitting control:', 'name': 'split_control', 'type': 'list', 'values': ['Screen spliting','GreyA','GreyB']},
-                 {'title': 'Splitting value:', 'name': 'split_value', 'type': 'float', 'value': 0.5, 'min': 0, 'max': 1},
-                 {'title': 'Grey A value:', 'name': 'greyA_value', 'type': 'int', 'value': 0, 'min': 0, 'max': 255},
-                 {'title': 'Grey B value:', 'name': 'greyB_value', 'type': 'int', 'value': 255, 'min': 0, 'max': 255},
-                 {'title': 'Splitting direction:', 'name': 'split_dir', 'type': 'list', 'values': ['Horizontal','Vertical']},
-                 {'title': 'Flipped?:', 'name': 'split_flip', 'type': 'bool', 'value': False},
-                 ]},
+    params = [{'title': 'Show Preview?:', 'name': 'show_preview', 'type': 'bool', 'value': False},
+              {'title': 'Shaping type:', 'name': 'shaping_type', 'type': 'list', 'value': HoloeyeControls.names()[2],
+               'values': HoloeyeControls.names()},
+              {'title': 'Splitting options:', 'name': 'splitting', 'type': 'group', 'visible': True, 'children': [
+                  {'title': 'Splitting control:', 'name': 'split_control', 'type': 'list',
+                   'values': ['Screen spliting', 'GreyA', 'GreyB']},
+                  {'title': 'Splitting value:', 'name': 'split_value', 'type': 'float', 'value': 0.5, 'min': 0,
+                   'max': 1},
+                  {'title': 'Grey A value:', 'name': 'greyA_value', 'type': 'int', 'value': 0, 'min': 0, 'max': 255},
+                  {'title': 'Grey B value:', 'name': 'greyB_value', 'type': 'int', 'value': 255, 'min': 0, 'max': 255},
+                  {'title': 'Splitting direction:', 'name': 'split_dir', 'type': 'list',
+                   'values': ['Horizontal', 'Vertical']},
+                  {'title': 'Flipped?:', 'name': 'split_flip', 'type': 'bool', 'value': False},
+              ]},
               {'title': 'Calibration:', 'name': 'calibration', 'type': 'group', 'children': [
-                    {'title': 'File name:', 'name': 'calib_file', 'type': 'browsepath', 'value': 'C:\\Users\\Weber\\Labo\\Programmes Python\\user_interface\\holography\\calibration\\calibration_phase_20180723.txt', 'filetype': True},
-                    {'title': 'Apply calib?:', 'name': 'calib_apply', 'type': 'bool', 'value': False},
-                    ]},
-              {'title': 'MultiAxes:', 'name': 'multiaxes', 'type': 'group','visible':is_multiaxes, 'children':[
-                        {'title': 'is Multiaxes:', 'name': 'ismultiaxes', 'type': 'bool', 'value': is_multiaxes, 'default': False},
-                        {'title': 'Status:', 'name': 'multi_status', 'type': 'list', 'value': 'Master', 'values': ['Master','Slave']},
-                        {'title': 'Axis:', 'name': 'axis', 'type': 'list',  'values':stage_names},
-                        
-                        ]}]+comon_parameters
+                  {'title': 'File name:', 'name': 'calib_file', 'type': 'browsepath',
+                   'value': 'C:\\Users\\Weber\\Labo\\Programmes Python\\user_interface\\holography\\calibration\\calibration_phase_20180723.txt',
+                   'filetype': True},
+                  {'title': 'Apply calib?:', 'name': 'calib_apply', 'type': 'bool', 'value': False},
+              ]},
+              {'title': 'MultiAxes:', 'name': 'multiaxes', 'type': 'group', 'visible': is_multiaxes, 'children': [
+                  {'title': 'is Multiaxes:', 'name': 'ismultiaxes', 'type': 'bool', 'value': is_multiaxes,
+                   'default': False},
+                  {'title': 'Status:', 'name': 'multi_status', 'type': 'list', 'value': 'Master',
+                   'values': ['Master', 'Slave']},
+                  {'title': 'Axis:', 'name': 'axis', 'type': 'list', 'values': stage_names},
+
+              ]}] + comon_parameters
 
     def __init__(self, parent=None, params_state=None):
         """
@@ -110,38 +117,37 @@ class DAQ_Move_Holoeye(DAQ_Move_base):
             # controller is an object that may be passed to other instances of DAQ_Move_Mock in case
             # of one controller controlling multiaxes
 
-            self.status.update(edict(info="",controller=None,initialized=False))
+            self.status.update(edict(info="", controller=None, initialized=False))
 
-
-            #check whether this stage is controlled by a multiaxe controller (to be defined for each plugin)
+            # check whether this stage is controlled by a multiaxe controller (to be defined for each plugin)
 
             # if mutliaxes then init the controller here if Master state otherwise use external controller
-            if self.settings.child('multiaxes','ismultiaxes').value() and self.settings.child('multiaxes','multi_status').value()=="Slave":
-                if controller is None: 
+            if self.settings.child('multiaxes', 'ismultiaxes').value() and self.settings.child('multiaxes',
+                                                                                               'multi_status').value() == "Slave":
+                if controller is None:
                     raise Exception('no controller has been defined externally while this axe is a slave one')
                 else:
-                    self.controller=controller
-            else: #Master stage
-                self.controller=slmdisplaysdk.SLMDisplay() #any object that will control the stages
-                
+                    self.controller = controller
+            else:  # Master stage
+                self.controller = slmdisplaysdk.SLMDisplay()  # any object that will control the stages
+
             dataWidth = self.controller.width_px
             dataHeight = self.controller.height_px
             self.data_uchar = slmdisplaysdk.createFieldUChar(dataWidth, dataHeight)
 
-            info="Holoeye"
-            self.status.info=info
-            self.status.controller=self.controller
-            self.status.initialized=True
+            info = "Holoeye"
+            self.status.info = info
+            self.status.controller = self.controller
+            self.status.initialized = True
             return self.status
 
         except Exception as e:
-            self.emit_status(ThreadCommand('Update_Status',[getLineInfo()+ str(e),'log']))
-            self.status.info=getLineInfo()+ str(e)
-            self.status.initialized=False
+            self.emit_status(ThreadCommand('Update_Status', [getLineInfo() + str(e), 'log']))
+            self.status.info = getLineInfo() + str(e)
+            self.status.initialized = False
             return self.status
 
-
-    def commit_settings(self,param):
+    def commit_settings(self, param):
         """
             | Activate any parameter changes on the PI_GCS2 hardware.
             |
@@ -149,49 +155,50 @@ class DAQ_Move_Holoeye(DAQ_Move_base):
 
         """
 
-        if param.name()=='shaping_type':
-            self.settings.child(('splitting')).show( param.value()=='SplitScreen') #splitting option
-            #set bounds
-            if param.value()=='FullScreen':
-                self.settings.child('bounds','is_bounds').setValue(True)
-                self.settings.child('bounds','max_bound').setValue(255)
+        if param.name() == 'shaping_type':
+            self.settings.child(('splitting')).show(param.value() == 'SplitScreen')  # splitting option
+            # set bounds
+            if param.value() == 'FullScreen':
+                self.settings.child('bounds', 'is_bounds').setValue(True)
+                self.settings.child('bounds', 'max_bound').setValue(255)
                 self.controller_units = 'greyscale'
 
-                #self.settings.child('
-            elif param.value()=='SplitScreen':
-                self.settings.child('bounds','is_bounds').setValue(True)
-                if self.settings.child('splitting', 'split_control').value()=='Screen spliting':
-                    self.settings.child('bounds','max_bound').setValue(1)
-                else: 
-                    self.settings.child('bounds','max_bound').setValue(255)
+                # self.settings.child('
+            elif param.value() == 'SplitScreen':
+                self.settings.child('bounds', 'is_bounds').setValue(True)
+                if self.settings.child('splitting', 'split_control').value() == 'Screen spliting':
+                    self.settings.child('bounds', 'max_bound').setValue(1)
+                else:
+                    self.settings.child('bounds', 'max_bound').setValue(255)
                 self.controller_units = 'pxl'
 
-            elif param.value()=='File':
-                self.settings.child('bounds','is_bounds').setValue(False)
+            elif param.value() == 'File':
+                self.settings.child('bounds', 'is_bounds').setValue(False)
                 self.controller_units = 'file'
 
-        elif param.name()=='show_preview':
+        elif param.name() == 'show_preview':
             self.controller.utilsSLMPreviewShow(param.value())
-             
-        elif param.parent().name()=='splitting':
-            if self.settings.child('splitting','split_control').value()=='Screen spliting':
-                self.settings.child('bounds','max_bound').setValue(1)
-            elif self.settings.child('splitting','split_control').value()=='GreyA' or self.settings.child('splitting','split_control').value()=='GreyB':
-                self.settings.child('bounds','max_bound').setValue(255)
 
-        elif param.name()=='calib_file' or param.name()=='calib_apply':
-            fname=self.settings.child('calibration', 'calib_file').value()
+        elif param.parent().name() == 'splitting':
+            if self.settings.child('splitting', 'split_control').value() == 'Screen spliting':
+                self.settings.child('bounds', 'max_bound').setValue(1)
+            elif self.settings.child('splitting', 'split_control').value() == 'GreyA' or self.settings.child(
+                    'splitting', 'split_control').value() == 'GreyB':
+                self.settings.child('bounds', 'max_bound').setValue(255)
+
+        elif param.name() == 'calib_file' or param.name() == 'calib_apply':
+            fname = self.settings.child('calibration', 'calib_file').value()
             self.load_calibration(fname)
 
 
     def load_calibration(self,fname):
         (root,ext)=os.path.splitext(fname)
         if 'h5' in ext:
-            self.calibration=h5browser.browse_data(fname) #phase values corresponding to grey levels (256 elements in array)
+            self.calibration = browse_data(fname) #phase values corresponding to grey levels (256 elements in array)
         elif 'txt' in ext or 'dat' in ext:
-            self.calibration=np.loadtxt(fname)[:,1] # to update in order to select what data in file
+            self.calibration = np.loadtxt(fname)[:,1] # to update in order to select what data in file
         else: 
-            self.calibration=None
+            self.calibration = None
             self.emit_status(ThreadCommand('Update_Status',['No calibration has been loaded','log']))
 
     def close(self):
